b538c0362d3163174d4fa3246aea80857b80e932
2300032527 <163747658+2300032527@users.noreply.github.com>
2025-10-06 09:12:13 +0530

    Update Signup.jsx

--- SDP-MUSIC-STREAMING-main/src/components/Signup.jsx
+++ SDP-MUSIC-STREAMING-main/src/components/Signup.jsx
@@ -13,7 +13,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:8080/api/signup", {
+      const response = await axios.post("http://localhost:2025/api/signup", {
         username,
         name,
         password,
