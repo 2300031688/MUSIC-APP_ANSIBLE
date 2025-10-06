3abc658faf259c9236e9a600ce9468ea52293129
2300032527 <163747658+2300032527@users.noreply.github.com>
2025-10-06 08:47:30 +0530

    Update Login.jsx 1

--- SDP-MUSIC-STREAMING-main/src/components/Login.jsx
+++ SDP-MUSIC-STREAMING-main/src/components/Login.jsx
@@ -9,13 +9,10 @@ const Login = () => {
   const [message, setMessage] = useState("");
   const navigate = useNavigate();
 
-  // Use API URL from .env
-  const API_URL = import.meta.env.VITE_API_URL;
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${API_URL}/api/login`, {
+      const response = await axios.post("http://localhost:8080/api/login", {
         username,
         password,
       });
